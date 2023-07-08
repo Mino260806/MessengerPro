ba816a3944b51056961aa14edd843a9b2f5266de
Mino260806 <mino260806@gmail.com>
2023-07-08 23:54:31 +0100

    slightly increased supported messenger version

--- app/src/main/java/tn/amin/mpro2/constants/ModuleInfo.java
+++ app/src/main/java/tn/amin/mpro2/constants/ModuleInfo.java
@@ -5,7 +5,7 @@ public class ModuleInfo {
     public static final String LINK_GITHUB_PAGE = "https://github.com/Mino260806/MessengerPro/";
     public static final String LINK_TELEGRAM_SUPPORT = "https://t.me/MProSupport/";
 
-    public static final int MIN_ORCA_VERSION = 313002715;
-    public static final int MAX_ORCA_VERSION = 313202299;
-    public static final String RECOMMENDED_ORCA_VERSION_STRING = "416";
+    public static final int MIN_ORCA_VERSION = 313202933;
+    public static final int MAX_ORCA_VERSION = 313203012;
+    public static final String RECOMMENDED_ORCA_VERSION_STRING = "416.0.0.9.76";
 }
