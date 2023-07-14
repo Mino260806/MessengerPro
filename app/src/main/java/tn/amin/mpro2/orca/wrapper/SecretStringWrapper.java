942c337d4dd500931c427bf5d582bb1b0a46c4a3
Mino260806 <mino260806@gmail.com>
2023-07-14 20:24:54 +0100

    ability to change received message content internally

--- app/src/main/java/tn/amin/mpro2/orca/wrapper/SecretStringWrapper.java
+++ app/src/main/java/tn/amin/mpro2/orca/wrapper/SecretStringWrapper.java
@@ -3,27 +3,39 @@ package tn.amin.mpro2.orca.wrapper;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import java.lang.ref.WeakReference;
+import java.lang.reflect.Field;
+
+import tn.amin.mpro2.hook.unobfuscation.OrcaUnobfuscator;
+import tn.amin.mpro2.orca.OrcaGateway;
+import tn.amin.mpro2.ui.WrapperHelper;
+import tn.amin.mpro2.util.StringUtil;
+
 public class SecretStringWrapper {
-    private final String content;
-    private final String stars;
+    private final OrcaGateway gateway;
+    private final WeakReference<Object> mObject;
 
-    public SecretStringWrapper(Parcelable secretString) {
-        Parcel parcel = Parcel.obtain();
+    private final Field mContentField;
+    private final Field mStarsField;
 
-        secretString.writeToParcel(parcel, 0);
-        parcel.setDataPosition(0);
+    public SecretStringWrapper(OrcaGateway gateway, Object secretString) {
+        this.gateway = gateway;
+        mObject = new WeakReference<>(secretString);
 
-        content = parcel.readString();
-        stars = parcel.readString();
-
-        parcel.recycle();
+        mContentField = gateway.unobfuscator.getField(OrcaUnobfuscator.FIELD_SECRET_STRING_CONTENT);
+        mStarsField = gateway.unobfuscator.getField(OrcaUnobfuscator.FIELD_SECRET_STRING_STARS);
     }
 
     public String getContent() {
-        return content;
+        return (String) WrapperHelper.fieldGet(mContentField, mObject.get());
     }
 
     public String getStars() {
-        return stars;
+        return (String) WrapperHelper.fieldGet(mStarsField, mObject.get());
+    }
+
+    public boolean setContent(String content) {
+        return WrapperHelper.fieldSet(mContentField, mObject.get(), content) &&
+                WrapperHelper.fieldSet(mStarsField, mObject.get(), StringUtil.multiply("*", content.length()));
     }
 }
