eafd9cf850dd6899daadeacef2ad0aabf379f03f
Mino260806 <mino260806@gmail.com>
2023-07-14 22:33:58 +0100

    added a new hook when a message is displayed

---
+++ app/src/main/java/tn/amin/mpro2/hook/all/MessagesDisplayHook.java
@@ -0,0 +1,48 @@
+package tn.amin.mpro2.hook.all;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
+import tn.amin.mpro2.debug.Logger;
+import tn.amin.mpro2.hook.BaseHook;
+import tn.amin.mpro2.hook.HookId;
+import tn.amin.mpro2.hook.unobfuscation.OrcaUnobfuscator;
+import tn.amin.mpro2.orca.OrcaGateway;
+import tn.amin.mpro2.orca.wrapper.MessageWrapper;
+import tn.amin.mpro2.orca.wrapper.MessagesCollectionWrapper;
+
+public class MessagesDisplayHook extends BaseHook {
+    @Override
+    public HookId getId() {
+        return HookId.MESSAGES_DISPLAY;
+    }
+
+    @Override
+    protected Set<XC_MethodHook.Unhook> injectInternal(OrcaGateway gateway) {
+        return Collections.singleton(XposedBridge.hookMethod(
+                gateway.unobfuscator.getMethod(OrcaUnobfuscator.METHOD_MESSAGES_DECODER_DECODE),
+                wrap(new XC_MethodHook() {
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                Object messagesCollection = param.getResult();
+                List<?> messageList = new MessagesCollectionWrapper(gateway, messagesCollection).getList();
+                for (int i=0; i<messageList.size(); i++) {
+                    Object message = messageList.get(i);
+                    MessageWrapper messageWrapper = new MessageWrapper(gateway, message);
+
+                    final int index = i;
+                    notifyListeners((listener) -> ((MessageDisplayHookListener) listener).onMessageDisplay(
+                            messageWrapper, index, messageList.size()));
+                }
+            }
+        })));
+    }
+
+    public interface MessageDisplayHookListener {
+        void onMessageDisplay(MessageWrapper wrapper, int index, int count);
+    }
+}
