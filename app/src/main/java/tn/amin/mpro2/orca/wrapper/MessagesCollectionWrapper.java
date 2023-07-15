b5142e29ff61f9820e53afbd0a475ccaf0105808
Mino260806 <mino260806@gmail.com>
2023-07-15 16:02:04 +0100

    more details in displayed message hook

--- app/src/main/java/tn/amin/mpro2/orca/wrapper/MessagesCollectionWrapper.java
+++ app/src/main/java/tn/amin/mpro2/orca/wrapper/MessagesCollectionWrapper.java
@@ -1,5 +1,8 @@
 package tn.amin.mpro2.orca.wrapper;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.lang.ref.WeakReference;
 import java.lang.reflect.Field;
 import java.util.List;
@@ -13,10 +16,25 @@ public class MessagesCollectionWrapper {
 
     private final Field mMessagesListField;
 
+    ThreadKeyWrapper mThreadKey;
+    boolean mIncludesFirstMessageInThread;
+    boolean mIncludesLastMessageInThread;
+
     public MessagesCollectionWrapper(OrcaGateway gateway, Object messagesCollection) {
         mObject = new WeakReference<>(messagesCollection);
 
         mMessagesListField = gateway.unobfuscator.getField(OrcaUnobfuscator.FIELD_MESSAGES_COLLECTION_MESSAGES);
+
+        Parcel parcel = Parcel.obtain();
+
+        ((Parcelable) messagesCollection).writeToParcel(parcel, 0);
+        parcel.setDataPosition(0);
+        mThreadKey = new ThreadKeyWrapper(parcel.readParcelable(gateway.classLoader));
+        parcel.setDataPosition(parcel.dataSize() - 4 * 2);
+        mIncludesFirstMessageInThread = parcel.readInt() == 1;
+        mIncludesLastMessageInThread = parcel.readInt() == 1;
+
+        parcel.recycle();
     }
 
     public List<?> getList() {
