c63aef2a03aa702220411c0a4c714ef34091c7d8
Loukious <Loukious@users.noreply.github.com>
2023-10-28 20:54:11 +0100

    Quality of life improvement to translate ui

--- app/src/main/java/tn/amin/mpro2/features/util/translate/TranslateConfigurationFrame.java
+++ app/src/main/java/tn/amin/mpro2/features/util/translate/TranslateConfigurationFrame.java
@@ -3,16 +3,24 @@ package tn.amin.mpro2.features.util.translate;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.LayoutInflater;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 
 import org.apache.commons.lang3.StringUtils;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Level;
 
 import tn.amin.mpro2.R;
 import tn.amin.mpro2.orca.OrcaGateway;
@@ -27,21 +35,40 @@ public class TranslateConfigurationFrame extends FrameLayout {
         LayoutInflater.from(context).inflate(R.layout.layout_translate_config, this);
 
         Button buttonSave = findViewById(R.id.button_save);
-        LinearLayout linearLayout = (LinearLayout) getChildAt(0);
         Switch receivedMessagesSwitch = findViewById(R.id.switch_received);
         Switch sentMessagesSwitch = findViewById(R.id.switch_sent);
         Switch keepOriginReceivedMessagesSwitch = findViewById(R.id.switch_keep_original_received);
         Switch keepOriginSentMessagesSwitch = findViewById(R.id.switch_keep_original_sent);
-
-        EditText sourceLanguageReceivedMessagesEdit = findViewById(R.id.edit_source_language_received);
-        EditText targetLanguageReceivedMessagesEdit = findViewById(R.id.edit_target_language_received);
-        EditText sourceLanguageSentMessagesEdit = findViewById(R.id.edit_source_language_sent);
-        EditText targetLanguageSentMessagesEdit = findViewById(R.id.edit_target_language_sent);
+        Spinner sourceLanguageReceivedMessagesSpinner = findViewById(R.id.spinner_source_language_received);
+        Spinner targetLanguageReceivedMessagesSpinner = findViewById(R.id.spinner_target_language_received);
+        Spinner sourceLanguageSentMessagesSpinner = findViewById(R.id.spinner_source_language_sent);
+        Spinner targetLanguageSentMessagesSpinner = findViewById(R.id.spinner_target_language_sent);
 
         TextView sourceLanguageReceivedMessagesText = findViewById(R.id.text_source_language_received);
         TextView targetLanguageReceivedMessagesText = findViewById(R.id.text_target_language_received);
         TextView sourceLanguageSentMessagesText = findViewById(R.id.text_source_language_sent);
         TextView targetLanguageSentMessagesText = findViewById(R.id.text_target_language_sent);
+        Animation shake = AnimationUtils.loadAnimation(this.getContext(), R.anim.shake_animation);
+        //Filling up the Source/Received spinner
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<>(this.getContext(), android.R.layout.simple_spinner_item, TranslationSupportedLanguages.languages);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sourceLanguageReceivedMessagesSpinner.setAdapter(adapter1);
+
+        //Filling up the Target/Received spinner
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<>(this.getContext(), android.R.layout.simple_spinner_item, TranslationSupportedLanguages.languages);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        targetLanguageReceivedMessagesSpinner.setAdapter(adapter2);
+
+        //Filling up the Source/Sent spinner
+        ArrayAdapter<String> adapter3 = new ArrayAdapter<>(this.getContext(), android.R.layout.simple_spinner_item, TranslationSupportedLanguages.languages);
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sourceLanguageSentMessagesSpinner.setAdapter(adapter3);
+
+        //Filling up the Target/Sent spinner
+        ArrayAdapter<String> adapter4 = new ArrayAdapter<>(this.getContext(), android.R.layout.simple_spinner_item, TranslationSupportedLanguages.languages);
+        adapter4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        targetLanguageSentMessagesSpinner.setAdapter(adapter4);
+
 
         receivedMessagesSwitch.setText(gateway.res.getString(R.string.received_messages));
         sentMessagesSwitch.setText(gateway.res.getString(R.string.sent_messages));
@@ -54,57 +81,34 @@ public class TranslateConfigurationFrame extends FrameLayout {
         keepOriginSentMessagesSwitch.setText(gateway.res.getString(R.string.keep_original_message));
 
         receivedMessagesSwitch.setOnCheckedChangeListener((s, checked) -> {
-            sourceLanguageReceivedMessagesEdit.setEnabled(checked);
-            targetLanguageReceivedMessagesEdit.setEnabled(checked);
+            sourceLanguageReceivedMessagesSpinner.setEnabled(checked);
+            targetLanguageReceivedMessagesSpinner.setEnabled(checked);
             keepOriginReceivedMessagesSwitch.setEnabled(checked);
         });
         sentMessagesSwitch.setOnCheckedChangeListener((s, checked) -> {
-            sourceLanguageSentMessagesEdit.setEnabled(checked);
-            targetLanguageSentMessagesEdit.setEnabled(checked);
+            sourceLanguageSentMessagesSpinner.setEnabled(checked);
+            targetLanguageSentMessagesSpinner.setEnabled(checked);
             keepOriginSentMessagesSwitch.setEnabled(checked);
         });
 
         buttonSave.setOnClickListener((b) -> {
             if (receivedMessagesSwitch.isChecked()) {
-                if (StringUtils.isBlank(targetLanguageReceivedMessagesEdit.getText().toString())) {
-                    targetLanguageReceivedMessagesEdit.setError("Mandatory field");
-                    return;
-                }
-
-                if (!StringUtils.isBlank(sourceLanguageReceivedMessagesEdit.getText()) &&
-                        !TranslationSupportedLanguages.isSupported(sourceLanguageReceivedMessagesEdit.getText().toString())) {
-                    sourceLanguageReceivedMessagesEdit.setError("Invalid language code");
-                    return;
-                }
-
-                if (!TranslationSupportedLanguages.isSupported(targetLanguageReceivedMessagesEdit.getText().toString())) {
-                    targetLanguageReceivedMessagesEdit.setError("Invalid language code");
+                if (targetLanguageReceivedMessagesSpinner.getSelectedItemPosition() < 1) {
+                    targetLanguageReceivedMessagesSpinner.startAnimation(shake);
                     return;
                 }
             }
 
             if (sentMessagesSwitch.isChecked()) {
-                if (StringUtils.isBlank(targetLanguageSentMessagesEdit.getText().toString())) {
-                    targetLanguageSentMessagesEdit.setError("Mandatory field");
-                    return;
-                }
-
-                if (!StringUtils.isBlank(sourceLanguageSentMessagesEdit.getText()) &&
-                        !TranslationSupportedLanguages.isSupported(sourceLanguageSentMessagesEdit.getText().toString())) {
-                    sourceLanguageSentMessagesEdit.setError("Invalid language code");
-                    return;
-                }
-
-                if (!TranslationSupportedLanguages.isSupported(targetLanguageSentMessagesEdit.getText().toString())) {
-                    targetLanguageSentMessagesEdit.setError("Invalid language code");
+                if (targetLanguageSentMessagesSpinner.getSelectedItemPosition() < 1) {
+                    targetLanguageSentMessagesSpinner.startAnimation(shake);
                     return;
                 }
             }
 
             if (receivedMessagesSwitch.isChecked()) {
-                String source = sourceLanguageReceivedMessagesEdit.getText().toString().trim();
-                if (StringUtils.isBlank(source)) source = "auto";
-                String target = targetLanguageReceivedMessagesEdit.getText().toString().trim();
+                String source = TranslationSupportedLanguages.languageCodes.get(sourceLanguageReceivedMessagesSpinner.getSelectedItemPosition());
+                String target = TranslationSupportedLanguages.languageCodes.get(targetLanguageReceivedMessagesSpinner.getSelectedItemPosition());
                 boolean keepOriginal = keepOriginReceivedMessagesSwitch.isChecked();
 
                 gateway.pref.addTranslatedConversationReceived(threadKey, new TranslationInfo(source, target, keepOriginal));
@@ -113,9 +117,8 @@ public class TranslateConfigurationFrame extends FrameLayout {
             }
 
             if (sentMessagesSwitch.isChecked()) {
-                String source = sourceLanguageSentMessagesEdit.getText().toString().trim();
-                if (StringUtils.isBlank(source)) source = "auto";
-                String target = targetLanguageSentMessagesEdit.getText().toString().trim();
+                String source = TranslationSupportedLanguages.languageCodes.get(sourceLanguageSentMessagesSpinner.getSelectedItemPosition());
+                String target = TranslationSupportedLanguages.languageCodes.get(targetLanguageSentMessagesSpinner.getSelectedItemPosition());
                 boolean keepOriginal = keepOriginSentMessagesSwitch.isChecked();
 
                 gateway.pref.addTranslatedConversationSent(threadKey, new TranslationInfo(source, target, keepOriginal));
@@ -131,28 +134,37 @@ public class TranslateConfigurationFrame extends FrameLayout {
 
         if (translationInfoReceived == null) {
             receivedMessagesSwitch.setChecked(false);
-            sourceLanguageReceivedMessagesEdit.setEnabled(false);
-            targetLanguageReceivedMessagesEdit.setEnabled(false);
+            sourceLanguageReceivedMessagesSpinner.setEnabled(false);
+            targetLanguageReceivedMessagesSpinner.setEnabled(false);
             keepOriginReceivedMessagesSwitch.setEnabled(false);
         } else {
             receivedMessagesSwitch.setChecked(true);
-            if (!translationInfoReceived.source.equals("auto")) sourceLanguageReceivedMessagesEdit.setText(translationInfoReceived.source);
-            targetLanguageReceivedMessagesEdit.setText(translationInfoReceived.target);
+            if (!translationInfoReceived.source.equals("auto")) sourceLanguageReceivedMessagesSpinner.setSelection(getIndexFromList(TranslationSupportedLanguages.languageCodes, translationInfoReceived.source));
+            targetLanguageReceivedMessagesSpinner.setSelection(getIndexFromList(TranslationSupportedLanguages.languageCodes, translationInfoReceived.target));
             keepOriginReceivedMessagesSwitch.setChecked(translationInfoReceived.keepOriginal);
         }
 
         if (translationInfoSent == null) {
             sentMessagesSwitch.setChecked(false);
-            sourceLanguageSentMessagesEdit.setEnabled(false);
-            targetLanguageSentMessagesEdit.setEnabled(false);
+            sourceLanguageSentMessagesSpinner.setEnabled(false);
+            targetLanguageSentMessagesSpinner.setEnabled(false);
             keepOriginSentMessagesSwitch.setEnabled(false);
         } else {
             sentMessagesSwitch.setChecked(true);
-            if (!translationInfoSent.source.equals("auto")) sourceLanguageSentMessagesEdit.setText(translationInfoSent.source);
-            targetLanguageSentMessagesEdit.setText(translationInfoSent.target);
+            if (!translationInfoSent.source.equals("auto")) sourceLanguageSentMessagesSpinner.setSelection(getIndexFromList(TranslationSupportedLanguages.languageCodes, translationInfoSent.source));
+            targetLanguageSentMessagesSpinner.setSelection(getIndexFromList(TranslationSupportedLanguages.languageCodes, translationInfoSent.target));
             keepOriginSentMessagesSwitch.setChecked(translationInfoSent.keepOriginal);
         }
     }
+    public static int getIndexFromList(List<String> list, String target) {
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).equals(target)) {
+                return i; // Found a match, return the index
+            }
+        }
+        return -1; // Not found
+    }
+
 
     public void setOnSaveListener(OnSaveListener listener) {
         mListener = listener;
