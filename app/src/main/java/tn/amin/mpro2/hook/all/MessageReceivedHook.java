74e4827a5231a98c914356f8bcf720cc8394c4a5
Mino260806 <mino260806@gmail.com>
2023-11-06 15:37:53 +0100

    fix message received hook

--- app/src/main/java/tn/amin/mpro2/hook/all/MessageReceivedHook.java
+++ app/src/main/java/tn/amin/mpro2/hook/all/MessageReceivedHook.java
@@ -1,8 +1,11 @@
 package tn.amin.mpro2.hook.all;
 
+import org.apache.commons.lang3.math.NumberUtils;
+
 import java.util.Set;
 
 import de.robv.android.xposed.XC_MethodHook;
+import tn.amin.mpro2.debug.Logger;
 import tn.amin.mpro2.hook.BaseHook;
 import tn.amin.mpro2.hook.HookId;
 import tn.amin.mpro2.hook.HookTime;
@@ -28,9 +31,15 @@ public class MessageReceivedHook extends BaseHook {
                     @Override
                     protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
                         long convThreadKey = (Long) param.args[2];
-                        String messageId = (String) param.args[5];
-                        String message = (String) param.args[7];
-                        long senderUserKey = (Long) param.args[8];
+                        String senderUserKeyString = (String) param.args[3];
+                        String messageId = (String) param.args[6];
+                        String message = (String) param.args[8];
+
+                        long senderUserKey = NumberUtils.toLong(senderUserKeyString, -1);
+                        if (senderUserKey == -1) {
+                            Logger.warn("Unknown user key: " + senderUserKeyString);
+                            return;
+                        }
 
                         notifyListeners((listener) ->
                                 ((MessageReceivedListener) listener).onMessageReceived(message, messageId, senderUserKey, convThreadKey));
