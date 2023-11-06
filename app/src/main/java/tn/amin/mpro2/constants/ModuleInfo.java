ba6a924b7c32b00ea1ed18b240f0bc6edcf1c1f8
Loukious <Loukious@users.noreply.github.com>
2023-11-06 14:44:37 +0100

    Bumping supported version and module version

--- app/src/main/java/tn/amin/mpro2/constants/ModuleInfo.java
+++ app/src/main/java/tn/amin/mpro2/constants/ModuleInfo.java
@@ -7,8 +7,8 @@ public class ModuleInfo {
     public static final String LINK_GITHUB_WIKI_USAGE_GUIDE = "https://github.com/Mino260806/MessengerPro/wiki/Usage-Guide/";
 
     public static final int MIN_ORCA_VERSION = 316208659;
-    public static final int MAX_ORCA_VERSION = 316408703;
-    public static final String RECOMMENDED_ORCA_VERSION_STRING = "432.0.0.39.118";
+    public static final int MAX_ORCA_VERSION = 316608148;
+    public static final String RECOMMENDED_ORCA_VERSION_STRING = "433.0.0.32.117";
 
     public static final String PACKAGE_AI_PLUGIN = "tn.amin.mproai";
 }
